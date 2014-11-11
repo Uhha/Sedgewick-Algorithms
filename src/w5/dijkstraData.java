65d9d32ef00d087be48b379354c4f84402c6101f
Uhha <asvinarenka@gmail.com>
2014-11-11 17:01:24 -0500

    w5done

--- src/w5/dijkstraData.java
+++ src/w5/dijkstraData.java
@@ -2,18 +2,19 @@ package w5;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
-
-import w4_1.Graph;
 
 public class dijkstraData {
 
 	private int[][] length;
 	private Node[] graph;
+	private int vertex;
 	
-	public dijkstraData(int size, String file) throws FileNotFoundException {
+	public dijkstraData(int size, String file, int vertex) throws FileNotFoundException {
+		
+		int indicator = 0;
+		this.vertex = vertex;
 		graph = new Node[size];
 		length = new int[size][size];
 		File f = new File(file);
@@ -22,23 +23,23 @@ public class dijkstraData {
 		while(sc.hasNextLine()){
 			String[] line = sc.nextLine().split("\t");
 			//System.out.println(Arrays.toString(line));
-			int id = Integer.parseInt(line[0]);
+			int id = Integer.parseInt(line[0]) + indicator;
 			graph[id] = new Node(id_counter);
 			for (int i = 1; i < line.length; i++) {
 				String[] numdist = line[i].split(",");
-				length[Integer.parseInt(numdist[0])][id] = Integer.parseInt(numdist[1]);
-				length[id][Integer.parseInt(numdist[0])] = Integer.parseInt(numdist[1]);
-				graph[id].next.add(Integer.parseInt(numdist[0]));
+				//length[Integer.parseInt(numdist[0]) + indicator][id] = Integer.parseInt(numdist[1]);
+				length[id][Integer.parseInt(numdist[0]) + indicator] = Integer.parseInt(numdist[1]);
+				graph[id].next.add(Integer.parseInt(numdist[0]) + indicator);
 			}
 			id_counter++;
 		}
-		graph[1].dist = 0;
+		graph[vertex].dist = 0;    //?? 1>29
 		sc.close();
 	}
 	
 	public void shotP(){
 		LinkedList<Node> ll = new LinkedList<>();
-		ll.add(graph[1]);
+		ll.add(graph[vertex]);    //?? 1>29
 		int cnt = 0;
 		while(ll.size() > 0){
 			cnt++;
@@ -49,6 +50,7 @@ public class dijkstraData {
 				int newdist = element.dist + length[element.id][nextnode];
 				if (newdist < graph[nextnode].dist){
 					graph[nextnode].dist = newdist;
+					graph[nextnode].visited = false;
 				}
 				
 				if(!graph[nextnode].visited){
@@ -93,17 +95,18 @@ public class dijkstraData {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		//dijkstraData dj = new dijkstraData(201, "src/w5/dijkstraData.txt");
-		dijkstraData dj = new dijkstraData(14+1, "src/w5/tc02.txt");
+		dijkstraData dj = new dijkstraData(200+1, "src/w5/dijkstraData.txt", 1);
 		
 		dj.shotP();
 		System.out.println(dj.toString());
-//		int[] vert = new int[]{7,37,59,82,99,115,133,165,188,197};
-//		for (int i = 0; i < vert.length; i++) {
-//			System.out.print(dj.graph[vert[i]].dist + ",");
-//		}
+		int[] vert = new int[]{7,37,59,82,99,115,133,165,188,197};
+		for (int i = 0; i < vert.length; i++) {
+			System.out.print(dj.graph[vert[i]].dist + ",");
+		}
 	}
 
-	
+	//2599,2610,2947,2052,2367,2399,2029,2442,2610,3068 wrong one;
+	//2599,2610,2947,2052,2367,2399,2029,2442,2505,3068 right one
 	
 	
 }
