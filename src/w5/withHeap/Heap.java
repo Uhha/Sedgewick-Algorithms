a4b69f2cd7290ca2e73af4163da1ab8b02241d4f
Uhha <asvinarenka@gmail.com>
2014-11-14 17:23:00 -0500

    heap

---
+++ src/w5/withHeap/Heap.java
@@ -0,0 +1,17 @@
+package w5.withHeap;
+
+public class Heap<T> {
+
+	private T[] ar;
+	
+	@SuppressWarnings("unchecked")
+	public Heap(){
+		ar = (T[]) new Object[10];
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
